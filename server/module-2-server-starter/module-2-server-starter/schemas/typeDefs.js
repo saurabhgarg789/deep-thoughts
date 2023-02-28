cd3998a05edc2a95c0339f2cfb82091571209b7c
Sowmiyaa Vasan <sowmiyaavasan@Sowmiyaas-MacBook-Pro.local>
2023-02-27 16:20:03 -0800

    Setup apollo server

---
+++ server/module-2-server-starter/module-2-server-starter/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+type Thought {
+    _id: ID
+  thoughtText: String
+  createdAt: String
+  username: String
+  reactionCount: Int
+  reactions: [Reaction]
+}
+
+type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+}
+
+type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
+type Query {
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+    users: [User]
+    user(username: String!): User
+}
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
